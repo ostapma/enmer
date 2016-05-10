a98f3f3ce3cbc4905c451230a0b74bf2fc6b6d87
ostapma <omagerya@gmail.com>
2016-05-10 16:23:45 +0300

    Login markup with angular-material

---
+++ web/EnmerWeb/EnmerWeb/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using EnmerWeb.Models;
+
+namespace EnmerWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View(new LoginModel());
+        }
+        [HttpPost, AllowAnonymous]
+        public ActionResult Login(LoginModel model)
+        {
+            return View(new LoginModel() { IsFailed = true });
+        }
+    }
+}
---
+++ web/EnmerWeb/EnmerWeb/Models/LoginModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnmerWeb.Models
+{
+    public class LoginModel
+    {
+        public string Email
+        {
+            get; set;
+        }
+
+        public string Password
+        {
+            get; set;
+        }
+
+        public bool IsFailed
+        {
+            get; set;
+        }
+    }
+}
