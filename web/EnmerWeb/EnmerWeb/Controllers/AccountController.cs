7ce3d54d4d9b364eb20bf38a014516cc7e45dc3c
ostapma <omagerya@gmail.com>
2016-05-11 09:56:38 +0300

    asp.net identiy

--- web/EnmerWeb/EnmerWeb/Controllers/AccountController.cs
+++ web/EnmerWeb/EnmerWeb/Controllers/AccountController.cs
@@ -3,22 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using EnmerWeb.Models;
 
 namespace EnmerWeb.Controllers
 {
     public class AccountController : Controller
     {
+
+        private ApplicationSignInManager signInManager;
+        private ApplicationUserManager userManager;
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                userManager = value;
+            }
+        }
+
         [AllowAnonymous]
         public ActionResult Login()
         {
             return View(new LoginModel());
         }
         [HttpPost, AllowAnonymous]
-        public ActionResult Login(LoginModel model)
+        public async Task< ActionResult> Login(LoginModel model, string returnUrl)
         {
-            return View(new LoginModel() { IsFailed = true });
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, change to shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, false, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.Failure:
+                default:
+                    return View(new LoginModel() { IsFailed = true });
+            }
+
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ web/EnmerWeb/EnmerWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnmerWeb.Startup))]
+    namespace EnmerWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
